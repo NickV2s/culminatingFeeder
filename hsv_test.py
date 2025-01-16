81e0202ef930a80d21db447caaa8e3ad85eb6052
NickV2s <souza.nicholas@outlook.com>
2025-01-15 20:38:46 -0500

    wip

--- hsv_test.py
+++ hsv_test.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 TIGER_DATA = "TigerData.txt"
 PENNY_DATA = "PennyData.txt"
+UNKNOWN_CAT = "Cat.txt"
 # import numpy as np
 def saveArray(filename:str,data:str):
     f = open(filename, "w")
@@ -14,26 +15,34 @@ def readArray(filename:str):
 img1 = cv2.imread("pennyTest.jpg")
 img2 = cv2.imread("pennyTest.jpg")
 face_cascade = cv2.CascadeClassifier("catfacesExtended.xml")
-gray = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+rgb = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
 #plt.figure()
 #gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
 #gray = cv2.resize(gray,(480,640))
-faces = face_cascade.detectMultiScale(gray,1.2,2,minSize=(30,30))
+faces = face_cascade.detectMultiScale(rgb,1.2,2,minSize=(30,30))
 
 for (x,y,w,h) in faces:  
-    cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.rectangle(rgb,(x,y),(x+w,y+h),(0,255,0),2)
     print(x,y,w,h)
-    cropped = gray[y:y+h,x:x+w]
+    cropped = rgb[y:y+h,x:x+w]
     centerX = int(w/2)
     centerY = int(h/2) 
     data = []
-    for i in range(1,50):
-        cropped[centerY,centerX+i] = (255,0,0)
-        tests = cropped[[centerX,centerY]]
-        hsv = cv2.cvtColor(tests, cv2.COLOR_RGB2HSV) 
-        print(hsv[0][0][2])
-        data.append(hsv[0][0][2])
-    saveArray(PENNY_DATA,str(data))
+    for row in range(centerX-25,centerX+25):
+        for col in range(centerY-25,centerY+25):
+            r, g, b = cropped[row, col]
+            print(f"Pixel {row*25+col} at (row {row}, col {col}): [{r}, {g}, {b}]")
+            data.append(f"[{r},{g},{b}]")
+            cropped[row,col] = (255,0,0)
+    # for i in range(1,50):
+    #     for j in range(1,50):
+    #         tests = cropped[[centerX+j,centerY+i]]
+    #         #hsv = cv2.cvtColor(tests, cv2.COLOR_RGB2HSV) 
+    #         print(tests[0][0])
+    #         data.append(tests[0][0])
+    #         cropped[centerY+j,centerX+i] = (255,0,0)
+
+    saveArray(UNKNOWN_CAT,str(data))
     print(len(faces))
 plt.imshow(cropped)
 plt.show()
